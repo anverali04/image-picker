ad8ece78f0d5b426039f7ccea22b41303732b7af
learnfromanver <63973978+learnfromanver@users.noreply.github.com>
2020-04-20 00:42:37 +0530

    Added properties folder

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace imagePicker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.openImageButton = new System.Windows.Forms.Button();
+            this.addToOutButton = new System.Windows.Forms.Button();
+            this.colorButton = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.saveOutButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(701, 536);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(744, 41);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(701, 536);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // openImageButton
+            // 
+            this.openImageButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openImageButton.Location = new System.Drawing.Point(13, 596);
+            this.openImageButton.Name = "openImageButton";
+            this.openImageButton.Size = new System.Drawing.Size(147, 50);
+            this.openImageButton.TabIndex = 1;
+            this.openImageButton.Text = "Open Image";
+            this.openImageButton.UseVisualStyleBackColor = true;
+            this.openImageButton.Click += new System.EventHandler(this.openImageButton_Click);
+            // 
+            // addToOutButton
+            // 
+            this.addToOutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addToOutButton.Location = new System.Drawing.Point(808, 590);
+            this.addToOutButton.Name = "addToOutButton";
+            this.addToOutButton.Size = new System.Drawing.Size(49, 50);
+            this.addToOutButton.TabIndex = 2;
+            this.addToOutButton.Text = "+";
+            this.addToOutButton.UseVisualStyleBackColor = true;
+            this.addToOutButton.Click += new System.EventHandler(this.AddToOutbutton_Click);
+            // 
+            // colorButton
+            // 
+            this.colorButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.colorButton.Location = new System.Drawing.Point(592, 590);
+            this.colorButton.Name = "colorButton";
+            this.colorButton.Size = new System.Drawing.Size(50, 50);
+            this.colorButton.TabIndex = 2;
+            this.colorButton.UseVisualStyleBackColor = true;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(648, 599);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(153, 69);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.trackBar1.Value = 10;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // saveOutButton
+            // 
+            this.saveOutButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saveOutButton.Location = new System.Drawing.Point(1250, 596);
+            this.saveOutButton.Name = "saveOutButton";
+            this.saveOutButton.Size = new System.Drawing.Size(195, 50);
+            this.saveOutButton.TabIndex = 2;
+            this.saveOutButton.Text = "Save Out Image";
+            this.saveOutButton.UseVisualStyleBackColor = true;
+            this.saveOutButton.Click += new System.EventHandler(this.saveOutButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1462, 658);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.colorButton);
+            this.Controls.Add(this.saveOutButton);
+            this.Controls.Add(this.addToOutButton);
+            this.Controls.Add(this.openImageButton);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button openImageButton;
+        private System.Windows.Forms.Button addToOutButton;
+        private System.Windows.Forms.Button colorButton;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Button saveOutButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace imagePicker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 990;
+        }
+
+Bitmap baseImage;
+Bitmap outImage;
+private void openImageButton_Click(object sender, EventArgs e)
+{
+    var openFileDialog = new OpenFileDialog();
+    openFileDialog.ShowDialog();
+
+    baseImage = new Bitmap(openFileDialog.FileName);
+
+    // Resize the image to fit in the picturebox
+    baseImage = new Bitmap(baseImage, pictureBox1.Width, pictureBox1.Height);
+            
+    pictureBox1.Image = baseImage;
+
+    // Create outImage with same base image size to store the output
+    outImage = new Bitmap(baseImage.Width, baseImage.Height);
+    var g = Graphics.FromImage(outImage);
+    // Fill the outImage with white color
+    g.FillRectangle(Brushes.White, 0, 0, outImage.Width, outImage.Height);
+}
+
+Bitmap workingImage;
+Color pickColour;
+private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+{
+    // Get the color of image in the mouse clicked location
+    pickColour = Color.FromArgb(baseImage.GetPixel(e.X, e.Y).ToArgb());
+
+    drawIntoWorkingImage();
+}
+
+private void drawIntoWorkingImage()
+{
+    workingImage = new Bitmap(outImage);
+                        
+    var range = trackBar1.Value;
+
+    colorButton.BackColor = pickColour;
+
+    // Compare all the pixels in the image with the picked color 
+    for (int i = 0; i < baseImage.Width; i++)
+    {
+        for (int j = 0; j < baseImage.Height; j++)
+        {
+            var pixel = baseImage.GetPixel(i, j);
+
+            // if the difference of the picked color and current pixel comes in range 
+            if (pixel.R - pickColour.R > -range && pixel.R - pickColour.R < range
+                && pixel.G - pickColour.G > -range && pixel.G - pickColour.G < range
+                && pixel.B - pickColour.B > -range && pixel.B - pickColour.B < range)
+            {
+                workingImage.SetPixel(i, j, pixel);
+            }
+        }
+    }
+
+    pictureBox2.Image = workingImage;
+}
+
+private void trackBar1_Scroll(object sender, EventArgs e)
+{
+    drawIntoWorkingImage();
+}
+
+private void AddToOutbutton_Click(object sender, EventArgs e)
+{
+    outImage = new Bitmap(workingImage);
+}
+
+private void saveOutButton_Click(object sender, EventArgs e)
+{
+    var saveFileDialog = new SaveFileDialog();
+    saveFileDialog.ShowDialog();
+
+    outImage.Save(saveFileDialog.FileName + ".jpeg", ImageFormat.Jpeg);
+}
+    }
+}
